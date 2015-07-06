be0f4b27df6ace3d3bacbcb06007aee97cafd443
Zyg0te <edvard.holst@gmail.com>
2015-07-06 15:16:43 +0200

    Added functionality for basic password generation
    - Now supports specifying range as absolute value or range
    - Custom character inclusion and character exclusion works
    - Write password to clipboard

--- popup.js
+++ popup.js
@@ -1,6 +1,64 @@
+function onPageDetailsReceived(pageDetails)  { 
+    document.getElementById('generated_password').value = pageDetails.password
+}
+
 function genPassword() {
     // Cancel the form submit
     event.preventDefault();
+
+    var pass = "";
+
+    //Get specified length or range
+    var passlengthval = document.getElementById('length').value
+    var maxlen = 10
+    var minlen = 10
+    var passlength = 10
+    //Make sure length is a number
+    if (passlengthval.value != "" && !isNaN(passlengthval))
+    {
+        passlength = parseInt(passlengthval)
+    } 
+
+    //Check if length is a range
+    if (passlengthval.includes("-"))
+    {
+        //Split into elements
+        var range = passlengthval.split("-")
+        minlen = parseInt(range[0])
+        maxlen = parseInt(range[1])
+        if (!isNaN(minlen) && !isNaN(maxlen)) 
+        {
+            //Pick a number from range
+            passlength = Math.floor(Math.random() * (maxlen - minlen + 1)) + minlen;
+        }
+        
+    }
+
+    //Get possible characters
+    var customchars = document.getElementById('custom').value
+    var excludechars = document.getElementById('exclude').value
+    //Add custom characters
+    var possible = customchars
+
+    //TODO: Add if check for numbers and uppercase/lowercase characters
+    possible += "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    //Remove exclusion characters
+    for (var i = 0, len = excludechars.length; i < len; i++) {
+        possible = possible.replace(excludechars[i], "");
+    }
+
+    for( var i=0; i < passlength; i++ )
+        pass += possible.charAt(Math.floor(Math.random() * possible.length));
+
+
+    var passoutput = document.getElementById('generated_password')
+    passoutput.value = pass;
+    passoutput.focus ();
+    passoutput.select ();
+    document.execCommand('copy');
+    passoutput.blur ();
+
 }
 
 // When the popup HTML has loaded
@@ -10,10 +68,5 @@ window.addEventListener('load', function(evt) {
     // Handle the password generation form submit event with our genpassword function
     document.getElementById('genpassword').addEventListener('submit', genPassword);
     // Get the event page
-    chrome.runtime.getBackgroundPage(function(eventPage) {
-        // Call the getPageInfo function in the event page, passing in 
-        // our onPageDetailsReceived function as the callback. This injects 
-        // content.js into the current tab's HTML
-        eventPage.getPageDetails(onPageDetailsReceived);
-    });
+    
 });
