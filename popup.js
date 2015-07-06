5ce78a70aa5c5b2276214a02f0c92ce2b9c10522
Zyg0te <edvard.holst@gmail.com>
2015-07-06 13:41:12 +0200

    Added initial project files.
    - Icon
    - The manifest file
    - popup.html
    - popup.js placeholder code

---
+++ popup.js
@@ -0,0 +1,19 @@
+function genPassword() {
+    // Cancel the form submit
+    event.preventDefault();
+}
+
+// When the popup HTML has loaded
+window.addEventListener('load', function(evt) {
+    // Cache a reference to the status display SPAN
+    statusDisplay = document.getElementById('status-display');
+    // Handle the password generation form submit event with our genpassword function
+    document.getElementById('genpassword').addEventListener('submit', genPassword);
+    // Get the event page
+    chrome.runtime.getBackgroundPage(function(eventPage) {
+        // Call the getPageInfo function in the event page, passing in 
+        // our onPageDetailsReceived function as the callback. This injects 
+        // content.js into the current tab's HTML
+        eventPage.getPageDetails(onPageDetailsReceived);
+    });
+});
